df9ba5c3742a2e736bdaa7e8473172ece38f9680
Luísa Gontijo <luisaporf@gmail.com>
2023-10-14 12:36:00 -0300

    identação+aspas_simples

--- source/dominios/User/Services/UserService.ts
+++ source/dominios/User/Services/UserService.ts
@@ -1,18 +1,18 @@
-import prisma from "../../../../client/client";
-import {Usuario} from '@prisma/client'
+import prisma from '../../../../client/client';
+import {Usuario} from '@prisma/client';
 
 class UserService{
-    async create(body:Usuario) {
-        const user = await prisma.usuario.create({
-            data: {
-                nome: body.nome,
-                email: body.email,
-                senha: body.senha,
-                foto: body.foto,
-                cargo: body.cargo
-            }
-        })
-    }
+	async create(body:Usuario) {
+		const user = await prisma.usuario.create({
+			data: {
+				nome: body.nome,
+				email: body.email,
+				senha: body.senha,
+				foto: body.foto,
+				cargo: body.cargo
+			}
+		})
+	}
 }
 
 export default new UserService
