4602907f4687fe54f0133aa7168910e65b8b47b8
Luísa Gontijo <luisaporf@gmail.com>
2023-10-21 15:55:11 -0300

    feat: controller artist

---
+++ source/dominios/Artist/Controllers/ArtistController.ts
@@ -0,0 +1,62 @@
+import { ParametroInvalido } from '../../../../errors/errors';
+import {Artist} from '@prisma/client';
+import ArtistService from '../Services/ArtistService';
+
+
+class ArtistController{
+	async create(body: Artist) {
+		try {
+			await ArtistService.create(body);
+			console.log('Artist created sucessfully');
+		} catch (error) {
+			if (error instanceof ParametroInvalido) {
+				console.log(error.message);
+			}
+		}
+	}
+
+	async getArtistbyId(wantedId: number){
+		try {
+			const artist = await ArtistService.getArtistById(wantedId);
+			return artist;
+		} catch (error) {
+			if (error instanceof ParametroInvalido) {
+				console.log(error.message);
+			}
+		}
+	}
+
+	async getArtists(){
+		try {
+			const artists = ArtistService.getArtists();
+			return artists;
+		} catch (error) {
+			if (error instanceof Error) {
+				console.log(error.message);
+			}
+		}
+	}
+
+	async update(wantedId: number, body: Artist){
+		try {
+			await ArtistService.update(wantedId,body);
+		} catch (error) {
+			if (error instanceof ParametroInvalido) {
+				console.log(error.message);
+			}
+		}
+	}
+    
+	async delete (wantedId: number){
+		try {
+			await ArtistService.delete(wantedId);
+			
+		}catch (error) {
+			if (error instanceof ParametroInvalido) {
+				console.log(error.message);
+			}
+		}
+	}
+}
+
+export default new ArtistController;
